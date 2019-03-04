f4ec13d4a07f82027f7b9bfa94d69d426fdee7ca
Kevin <kevingm31@gmail.com>
2019-03-04 11:26:15 -0600

    Move working code to new express app

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var resume = require('./routes/resume');
+var videos = require('./routes/videos');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,9 +22,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'partials')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+app.get('/resume', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/resume.html'))
+});
+app.get('/videos', function(req, res) {
+  res.sendFile(path.join(__dirname + '/views/videos.html'))
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
